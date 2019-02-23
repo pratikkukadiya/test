3f83f6dcf2723f839368f349057518ce342a69ea
pratikkukadiya <pratik.ios.devloper@gmail.com>
2019-02-23 21:55:07 +0530

    add new vc2

---
+++ test/ViewController2.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Pratik kukadiya on 7/8/17.
+//  Copyright © 2017 Pratik kukadiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // commit by Govind  test test test test
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
